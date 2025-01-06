02227a1508c82a3dfd7732df924898e8f6e34e19
Nivin34 <nivinselvaraj@gmail.com>
2025-01-06 10:26:47 +0530

    google tag

--- frontend/src/components/user/Profile.js
+++ frontend/src/components/user/Profile.js
@@ -8,7 +8,7 @@ export default function Profile () {
         <div className="row justify-content-around mt-5 user-info">
             <div className="col-12 col-md-3">
                 <figure className='avatar avatar-profile'>
-                    <img className="rounded-circle img-fluid" src={user.avatar??'./images/default_avatar.png'} alt='' />
+                    <img className="rounded-circle img-fluid" src={user.avatar??'/images/default_avatar.png'} alt='' />
                 </figure>
                 <Link to="/myprofile/update" id="edit_profile" className="btn btn-primary btn-block my-5">
                     Edit Profile
